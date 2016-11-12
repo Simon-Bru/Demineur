c324abd13fa674b515a00866674495588fe18060
simon bruguerolle <simon bruguerolle>
2016-11-12 21:42:09 +0100

    Ajout d'inclusions et initialisation de la graine aléatoire

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
 #include "plateau.h"
 #include "jeu.h"
+#include "case.h"
 
 /// MENU PRINCIPAL
 int main()
 {
+    /// Initialisation de la graine utilisée lors de l'appel à rand() afin de générer un nombre aléatoire en fonction du temps
+    srand(time(0));
+
     mainMenu();
 }
